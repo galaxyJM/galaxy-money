5f273bc1c6a665c84dabdc08120c24e1c5eed5a1
galaxyjiang <876708058@qq.com>
2021-10-27 19:52:25 +0800

    引入整个目录 封装全局ICON

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,26 +1,51 @@
 <template>
   <div class="nav">
-    <router-link to = "/labels">标签</router-link>
-    <router-link to = "/money">记一笔</router-link>
-    <router-link to = "/statistics">统计</router-link>
+    <router-link to="/labels">
+      <Icon name="labels"/>
+      <div>标签</div>
+    </router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      <div>记一笔</div>
+    </router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      <div>统计</div>
+    </router-link>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+// import x from '@/assets/icons/money.svg'
+// console.log(x)
+// 常规的引入方法
+
+// 一次性引入整个目录
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => {
+  requireContext.keys().forEach(requireContext);
+}
+
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
 export default {
   name: "Nav"
 }
 </script>
 
 <style scoped>
-    .nav{
-      display: flex;
-      justify-content: space-between;
-    }
-    a{
-      text-decoration: none;
-      color: black;
-    }
+.nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
 
 
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')  //引入nodejs的path模块来处理目录
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config =>{
+    const dir = path.resolve(__dirname,'src/assets/icons')  //找到要引入的svg的目录
+
+    config.module
+        .rule('svg-sprite')
+        .test(/\.svg$/)
+        .include.add(dir).end()   //定义一个规则 包含icons
+        .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite:true}])  //配置插件
+
+    config.module.rule('svg').exclude.add(dir)  //让其他的svg不使用上述规则
+  }
+
 }
