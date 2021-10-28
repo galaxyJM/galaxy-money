8263b751c6c0e044f7273699da2e0da9e4f42035
galaxyjiang <876708058@qq.com>
2021-10-28 19:36:10 +0800

    html

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,36 @@
 <template>
   <Layout>
-    money
+    <div class="types">
+      <ul>
+        <li>支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+
+    <div class="labels">
+      <ul>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+      <button>
+        新增标签
+      </button>
+    </div>
+    
+    <div class="notes">
+      <label>
+        写点备注
+        <input type="text">
+      </label>
+    </div>
+
+    <div class="numberPad">
+
+    </div>
+
+
   </Layout>
 
 </template>
