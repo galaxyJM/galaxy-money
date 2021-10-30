769c82ab118756261cab0051a5b06b4769342f7a
galaxyjiang <876708058@qq.com>
2021-10-30 10:51:56 +0800

    用ts写数字键盘组件 numberpad

--- src/components/NumberPad.vue
+++ src/components/NumberPad.vue
@@ -1,34 +1,70 @@
 <template>
   <div class="numberPad">
-    <div class="number">100</div>
+    <div class="number">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <button @click="inputNum">1</button>
+      <button @click="inputNum">2</button>
+      <button @click="inputNum">3</button>
+      <button @click="backspace">删除</button>
+      <button @click="inputNum">4</button>
+      <button @click="inputNum">5</button>
+      <button @click="inputNum">6</button>
+      <button @click="clean">清空</button>
+      <button @click="inputNum">7</button>
+      <button @click="inputNum">8</button>
+      <button @click="inputNum">9</button>
       <button class="ok">完成</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="inputNum" class="zero">0</button>
+      <button @click="inputNum">.</button>
 
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "NumberPad"
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from "vue-property-decorator";
+
+@Component
+export default class NumberPad extends Vue {
+  output = '0';
+  inputNum(event: MouseEvent) {
+    const button = (event.target as HTMLButtonElement); //强制执行类型，这样就无需判断e.target是否为空（ts会检查）
+    const input = button.textContent as string;  //理由同上 或直接加！ 写button.textContent！ 排除空的情况
+    if (this.output.length === 18) {
+      return;
+    }
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(input) >= 0)   //该判断条件表示input这个字符是否属于前面这个字符串  indexOf返回索引
+      {
+        this.output = input;
+        return;
+      }
+    }
+    if (this.output.indexOf('.') >= 0 && input === '.') {
+      return;
+    }
+    this.output += input;
+    console.log(1);
+  }
+
+  backspace() {
+    if (this.output.length === 1) {
+      this.output = '0';
+    } else {
+      this.output = this.output.slice(0, -1);
+    }
+  }
+
+  clean() {
+    this.output = '0';
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/style/reset.scss";
+
 .numberPad {
   margin-top: 10px;
 
