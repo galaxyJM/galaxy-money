24cccf0a5260289e6a4798f50d8862d5b02a0d2a
galaxyjiang <876708058@qq.com>
2021-11-07 17:45:18 +0800

    删除一些文件

--- src/store/recordListStore.ts
+++
@@ -1,8 +0,0 @@
-import recordListModel from "@/models/recordListModel";
-
-export default {
-    recordList: recordListModel.fetch(),  //将数据提升到全局
-    createRecord: (record: RecordItem) => {
-        recordListModel.createItem(record);
-    }
-}
